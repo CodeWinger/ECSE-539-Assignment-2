1c25331d0db188febf3515014da69ba7bdddfac0
CodeWinger <mathieu.boucher5@gmail.com>
2016-11-09 22:12:57 -0500

    fixed bug

--- src/ca/mcgill/ecse539/btms/model/Bus.java
+++ src/ca/mcgill/ecse539/btms/model/Bus.java
@@ -4,7 +4,7 @@
 package ca.mcgill.ecse539.btms.model;
 import java.util.*;
 
-// line 57 "../../../../../model.ump"
+// line 55 "../../../../../model.ump"
 public class Bus
 {
 
--- src/ca/mcgill/ecse539/btms/model/Driver.java
+++ src/ca/mcgill/ecse539/btms/model/Driver.java
@@ -4,7 +4,7 @@
 package ca.mcgill.ecse539.btms.model;
 import java.util.*;
 
-// line 70 "../../../../../model.ump"
+// line 68 "../../../../../model.ump"
 public class Driver
 {
 
